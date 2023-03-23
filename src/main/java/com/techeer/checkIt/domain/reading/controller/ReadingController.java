69974662453d1d834f49641c659660ca84ffbee6
jiminsong490 <misosjm@naver.com>
2023-03-24 00:17:10 +0900

    feat : ReadingController 생성

---
+++ src/main/java/com/techeer/checkIt/domain/reading/controller/ReadingController.java
@@ -0,0 +1,30 @@
+package com.techeer.checkIt.domain.reading.controller;
+
+import com.techeer.checkIt.domain.book.entity.Book;
+import com.techeer.checkIt.domain.book.service.BookService;
+import com.techeer.checkIt.domain.reading.dto.request.CreateReadingRequest;
+import com.techeer.checkIt.domain.reading.service.ReadingService;
+import com.techeer.checkIt.domain.user.entity.User;
+import com.techeer.checkIt.domain.user.service.UserService;
+import io.swagger.annotations.Api;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+@Api(tags = "독서현황 API")
+@RestController
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@RequestMapping("/api/v1/readings")
+public class ReadingController {
+    private final ReadingService readingService;
+    private final UserService userService;
+    private final BookService bookService;
+
+    @PostMapping("/{uid}")
+    public void createReading(@PathVariable Long uid, @RequestBody CreateReadingRequest.CreateReading readingDto) {
+        User user = userService.findById(uid);
+        Book book = bookService.findById(readingDto.getBookId());
+        readingService.registorReading(user, book, readingDto);
+    }
+}
