8305c8011eeb91d4fc0367955859d16a641770ca
jiminsong490 <misosjm@naver.com>
2023-03-29 23:56:59 +0900

    feat : 마지막 페이지 갱신 중 관련된 Reading 데이터 갱신 기능 추가

--- src/main/java/com/techeer/checkIt/domain/reading/repository/ReadingRepository.java
+++ src/main/java/com/techeer/checkIt/domain/reading/repository/ReadingRepository.java
@@ -1,7 +1,12 @@
 package com.techeer.checkIt.domain.reading.repository;
 
+import com.techeer.checkIt.domain.book.entity.Book;
 import com.techeer.checkIt.domain.reading.entity.Reading;
+import com.techeer.checkIt.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReadingRepository extends JpaRepository<Reading,Long> {
+    Optional<Reading> findLastPageByUserAndBook(User user, Book book);
 }
--- src/main/java/com/techeer/checkIt/domain/reading/service/ReadingService.java
+++ src/main/java/com/techeer/checkIt/domain/reading/service/ReadingService.java
@@ -5,23 +5,25 @@ import com.techeer.checkIt.domain.reading.dto.request.CreateReadingRequest;
 import com.techeer.checkIt.domain.reading.entity.Reading;
 import com.techeer.checkIt.domain.reading.entity.ReadingStatus;
 import com.techeer.checkIt.domain.reading.repository.ReadingRepository;
+import com.techeer.checkIt.domain.readingVolume.entity.ReadingVolume;
+import com.techeer.checkIt.domain.readingVolume.service.ReadingVolumeService;
+import com.techeer.checkIt.domain.user.dto.request.UpdateReadingRequestDto;
 import com.techeer.checkIt.domain.user.entity.User;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.time.LocalDateTime;
-import java.util.Date;
+import java.time.LocalDate;
 
 @Service
 @Transactional
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReadingService {
     private final ReadingRepository readingRepository;
+    private final ReadingVolumeService readingVolumeService;
 
     public void registerReading(User user, Book book, CreateReadingRequest createRequest){
-        LocalDateTime date = LocalDateTime.now();
         ReadingStatus status = ReadingStatus.convert(createRequest.getStatus().toUpperCase());
         int lastPage = 0;
         if(status == ReadingStatus.READING) lastPage = createRequest.getLastPage();
@@ -30,10 +32,25 @@ public class ReadingService {
         Reading reading = Reading.builder()
                 .user(user)
                 .book(book)
-                .date(date)
                 .lastPage(lastPage)
                 .status(status)
                 .build();
         readingRepository.save(reading);
+    }
+
+    public void findReadingByDate(User user, Book book, UpdateReadingRequestDto dto) {
+        LocalDate date = LocalDate.now();
+        Reading reading = readingRepository.findLastPageByUserAndBook(user,book).orElseThrow(null);
+        int pages = dto.getLastPages() - reading.getLastPage(); // 책A 읽은 페이지
+        if(readingVolumeService.existsUserAndDate(user,date)) { // 오늘 데이터가 있다면
+            ReadingVolume readingVolume = readingVolumeService.findReadingVolumeByUserAndDate(user, date);
+            readingVolume.sumTodayPages(pages); // 오늘 데이터에 더하기
+
+        }
+        else { // 없다면
+            readingVolumeService.registerReadingVolume(user, pages); // 새로 생성
+        }
+        reading.updateStatus(ReadingStatus.READING);
+        reading.updateLastPage(dto.getLastPages()); // reading의 lastpages 갱신
     }
 }
