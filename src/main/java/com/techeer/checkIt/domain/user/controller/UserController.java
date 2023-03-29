c71d7bd0af5cbe15fba40d42310a2599f4156804
jiminsong490 <misosjm@naver.com>
2023-03-29 23:55:51 +0900

    feat : 마지막 페이지 갱신 기능 추가

--- src/main/java/com/techeer/checkIt/domain/user/controller/UserController.java
+++ src/main/java/com/techeer/checkIt/domain/user/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.techeer.checkIt.domain.user.controller;
 
+import com.techeer.checkIt.domain.book.entity.Book;
+import com.techeer.checkIt.domain.book.service.BookService;
+import com.techeer.checkIt.domain.reading.service.ReadingService;
+import com.techeer.checkIt.domain.user.dto.request.UpdateReadingRequestDto;
+import com.techeer.checkIt.domain.user.entity.User;
 import com.techeer.checkIt.domain.user.service.UserService;
 import io.swagger.annotations.Api;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.time.LocalDate;
 
 @Api(tags = "회원 API")
 @RestController
@@ -15,7 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
+    private final ReadingService readingService;
+    private final BookService bookService;
 
+    @PutMapping("/readings/{uid}")
+    public void updateReading(@PathVariable Long uid, @RequestBody UpdateReadingRequestDto body) {
+        LocalDate date = LocalDate.now();
+        User user = userService.findUserById(uid);
+        Book book = bookService.findById(body.getBookId());
+        readingService.findReadingByDate(user,book,body);
+    }
     @PostMapping
     public void signup() {
         userService.join();
