5cd1aed43e3ab2e185680c57dc867977d334a427
jiminsong490 <misosjm@naver.com>
2023-03-29 23:52:20 +0900

    feat : ReadingVolume service 생성

---
+++ src/main/java/com/techeer/checkIt/domain/readingVolume/service/ReadingVolumeService.java
@@ -0,0 +1,37 @@
+package com.techeer.checkIt.domain.readingVolume.service;
+
+import com.techeer.checkIt.domain.reading.repository.ReadingRepository;
+import com.techeer.checkIt.domain.readingVolume.entity.ReadingVolume;
+import com.techeer.checkIt.domain.readingVolume.repository.ReadingVolumeRepository;
+import com.techeer.checkIt.domain.user.entity.User;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+
+@Service
+@Transactional
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReadingVolumeService {
+    private final ReadingVolumeRepository readingVolumeRepository;
+
+    public void registerReadingVolume(User user, int pages){
+        LocalDate date = LocalDate.now();
+        ReadingVolume readingVolume = ReadingVolume.builder()
+                .user(user)
+                .date(date)
+                .todayPages(pages)
+                .build();
+        readingVolumeRepository.save(readingVolume);
+    }
+
+    public ReadingVolume findReadingVolumeByUserAndDate(User user, LocalDate date) {
+        return readingVolumeRepository.findReadingVolumeByUserAndDate(user, date).orElseThrow();
+    }
+
+    public boolean existsUserAndDate(User user, LocalDate date) {
+        return readingVolumeRepository.existsByUserAndDate(user, date);
+    }
+}
