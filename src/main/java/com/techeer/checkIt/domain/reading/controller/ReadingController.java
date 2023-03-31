6eca7648736059b16886ab6ff9e950230b613fb8
jiminsong490 <misosjm@naver.com>
2023-03-31 20:16:36 +0900

    refactor : 마지막 페이지 갱신 API를 ReadingController로 이동

--- src/main/java/com/techeer/checkIt/domain/reading/controller/ReadingController.java
+++ src/main/java/com/techeer/checkIt/domain/reading/controller/ReadingController.java
@@ -3,12 +3,16 @@ package com.techeer.checkIt.domain.reading.controller;
 import com.techeer.checkIt.domain.book.entity.Book;
 import com.techeer.checkIt.domain.book.service.BookService;
 import com.techeer.checkIt.domain.reading.dto.request.CreateReadingRequest;
+import com.techeer.checkIt.domain.reading.dto.request.UpdateReadingAndReadingVolumeRequestDto;
+import com.techeer.checkIt.domain.reading.dto.response.UpdateReadingAndReadingVolumeResponseDto;
 import com.techeer.checkIt.domain.reading.service.ReadingService;
+import com.techeer.checkIt.domain.readingVolume.entity.ReadingVolume;
 import com.techeer.checkIt.domain.user.entity.User;
 import com.techeer.checkIt.domain.user.service.UserService;
 import io.swagger.annotations.Api;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -20,6 +24,14 @@ public class ReadingController {
     private final ReadingService readingService;
     private final UserService userService;
     private final BookService bookService;
+
+    @PutMapping("/readings/{uid}")
+    public ResponseEntity<UpdateReadingAndReadingVolumeResponseDto> updateReadingAndReadingVolume(@PathVariable Long uid, @RequestBody UpdateReadingAndReadingVolumeRequestDto body) {
+        User user = userService.findUserById(uid);
+        Book book = bookService.findById(body.getBookId());
+        ReadingVolume readingVolume = readingService.updateReadingAndReadingVolume(user,book,body);
+        return ResponseEntity.ok(UpdateReadingAndReadingVolumeResponseDto.of(body,readingVolume));
+    }
 
     @PostMapping("/{uid}")
     public void createReading(@PathVariable Long uid, @RequestBody CreateReadingRequest readingDto) {
--- src/main/java/com/techeer/checkIt/domain/user/controller/UserController.java
+++ src/main/java/com/techeer/checkIt/domain/user/controller/UserController.java
@@ -1,16 +1,10 @@
 package com.techeer.checkIt.domain.user.controller;
 
-import com.techeer.checkIt.domain.book.entity.Book;
-import com.techeer.checkIt.domain.book.service.BookService;
-import com.techeer.checkIt.domain.reading.service.ReadingService;
-import com.techeer.checkIt.domain.user.dto.request.UpdateReadingRequestDto;
-import com.techeer.checkIt.domain.user.entity.User;
 import com.techeer.checkIt.domain.user.service.UserService;
 import io.swagger.annotations.Api;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import java.time.LocalDate;
 
 @Api(tags = "회원 API")
 @RestController
@@ -18,16 +12,7 @@ import java.time.LocalDate;
 @RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
-    private final ReadingService readingService;
-    private final BookService bookService;
 
-    @PutMapping("/readings/{uid}")
-    public void updateReading(@PathVariable Long uid, @RequestBody UpdateReadingRequestDto body) {
-        LocalDate date = LocalDate.now();
-        User user = userService.findUserById(uid);
-        Book book = bookService.findById(body.getBookId());
-        readingService.findReadingByDate(user,book,body);
-    }
     @PostMapping
     public void signup() {
         userService.join();
