3bdb31fb29aa5102f0d5a6c988263bf7a6e66246
jiminsong490 <misosjm@naver.com>
2023-04-04 01:21:40 +0900

    feat : 오늘 날짜로부터 6일 전까지의 ReadingVolume을 가져오는 API 구현

--- src/main/java/com/techeer/checkIt/domain/readingVolume/repository/ReadingVolumeRepository.java
+++ src/main/java/com/techeer/checkIt/domain/readingVolume/repository/ReadingVolumeRepository.java
@@ -6,9 +6,11 @@ import com.techeer.checkIt.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface ReadingVolumeRepository extends JpaRepository<ReadingVolume,Long> {
+    List<ReadingVolume> findReadingVolumesByUserAndDateBetween(User user,LocalDate start, LocalDate end);
     Optional<ReadingVolume> findReadingVolumeByUserAndDate(User user, LocalDate date);
     boolean existsByUserAndDate(User user, LocalDate date);
 }
--- src/main/java/com/techeer/checkIt/domain/readingVolume/service/ReadingVolumeService.java
+++ src/main/java/com/techeer/checkIt/domain/readingVolume/service/ReadingVolumeService.java
@@ -1,8 +1,10 @@
 package com.techeer.checkIt.domain.readingVolume.service;
 
 import com.techeer.checkIt.domain.reading.repository.ReadingRepository;
+import com.techeer.checkIt.domain.readingVolume.dto.response.SearchReadingVolumesRes;
 import com.techeer.checkIt.domain.readingVolume.entity.ReadingVolume;
 import com.techeer.checkIt.domain.readingVolume.exception.ReadingVolumeNotFoundException;
+import com.techeer.checkIt.domain.readingVolume.mapper.ReadingVolumeMapper;
 import com.techeer.checkIt.domain.readingVolume.repository.ReadingVolumeRepository;
 import com.techeer.checkIt.domain.user.entity.User;
 import lombok.AccessLevel;
@@ -11,12 +13,23 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReadingVolumeService {
     private final ReadingVolumeRepository readingVolumeRepository;
+    private final ReadingVolumeMapper readingVolumeMapper;
+
+
+    public List<SearchReadingVolumesRes> findReadingVolumesByUserAndDateBetween(User user) {
+        LocalDate end = LocalDate.now();
+        LocalDate start = end.minusDays(6);
+        List<ReadingVolume> readingVolumeList =
+                readingVolumeRepository.findReadingVolumesByUserAndDateBetween(user,start,end);
+        return readingVolumeMapper.toDtoList(readingVolumeList);
+    }
 
     public ReadingVolume registerReadingVolume(User user, int pages){
         LocalDate date = LocalDate.now();
