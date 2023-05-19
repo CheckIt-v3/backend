885437d5729578196ad0b686158dae2d5e98d5ba
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-05-19 09:55:48 +0900

    test: book controller 테스트코드 작성

--- src/test/java/com/techeer/checkIt/fixture/BookFixtures.java
+++ src/test/java/com/techeer/checkIt/fixture/BookFixtures.java
@@ -1,12 +1,21 @@
 package com.techeer.checkIt.fixture;
 
 import com.techeer.checkIt.domain.book.dto.Response.BookRes;
-
 public class BookFixtures {
     public static final BookRes TEST_BOOK =
             BookRes.builder()
                     .author("작가")
                     .title("재미있는 책")
+                    .coverImageUrl("imageurl.com")
+                    .height(300)
+                    .width(200)
+                    .thickness(20)
+                    .publisher("일이삼 출판사")
+                    .build();
+    public static final BookRes TEST_BOOK2 =
+            BookRes.builder()
+                    .author("작가")
+                    .title("재미있는 북")
                     .coverImageUrl("imageurl.com")
                     .height(300)
                     .width(200)
