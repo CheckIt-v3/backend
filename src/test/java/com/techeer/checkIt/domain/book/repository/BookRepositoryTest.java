439548b3329bfaa426428cb63156bd83bf894398
jiminsong490 <misosjm@naver.com>
2023-06-02 22:11:50 +0900

    test : 테스트 결과 확인3

--- src/test/java/com/techeer/checkIt/domain/book/repository/BookRepositoryTest.java
+++ src/test/java/com/techeer/checkIt/domain/book/repository/BookRepositoryTest.java
@@ -1,23 +1,23 @@
-//package com.techeer.checkIt.domain.book.repository;
-//
-//import com.techeer.checkIt.domain.book.entity.Book;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import java.util.List;
-//
-//import static com.techeer.checkIt.fixture.BookFixtures.*;
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//
-//@DataJpaTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//class BookRepositoryTest {
-//
-//    @Autowired
-//    BookRepository bookRepository;
-//
+package com.techeer.checkIt.domain.book.repository;
+
+import com.techeer.checkIt.domain.book.entity.Book;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.util.List;
+
+import static com.techeer.checkIt.fixture.BookFixtures.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BookRepositoryTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
 //    @Test
 //    @DisplayName("Repository) 책 검색")
 //    void findByTitle() {
@@ -33,4 +33,4 @@
 //        // then
 //        assertThat(books.get(0).getTitle()).as("책검색 결과 %s, 검색한 책 : %s",books.get(0).getTitle(), book2.getTitle()).isEqualTo(book.getTitle());
 //    }
-//}
+}
