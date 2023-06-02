94cde10e7203cf9d2c891c79e731a50a497caa6a
jiminsong490 <misosjm@naver.com>
2023-06-02 22:15:28 +0900

    test : 테스트 결과 확인3

--- src/test/java/com/techeer/checkIt/domain/book/repository/BookRepositoryTest.java
+++ src/test/java/com/techeer/checkIt/domain/book/repository/BookRepositoryTest.java
@@ -18,19 +18,19 @@ class BookRepositoryTest {
     @Autowired
     BookRepository bookRepository;
 
-//    @Test
-//    @DisplayName("Repository) 책 검색")
-//    void findByTitle() {
-//
-//        // given
-//        Book book = bookRepository.save(TEST_BOOK_ENT);  // 세이노
-//        Book book2 = bookRepository.save(TEST_BOOK_ENT); // 불편한 편의점
-//        String keyword = "세이노";
-//
-//        // when
-//        List<Book> books = bookRepository.findByTitle(keyword);
-//
-//        // then
-//        assertThat(books.get(0).getTitle()).as("책검색 결과 %s, 검색한 책 : %s",books.get(0).getTitle(), book2.getTitle()).isEqualTo(book.getTitle());
-//    }
+    @Test
+    @DisplayName("Repository) 책 검색")
+    void findByTitle() {
+
+        // given
+        Book book = bookRepository.save(TEST_BOOK_ENT);  // 세이노
+        Book book2 = bookRepository.save(TEST_BOOK_ENT); // 불편한 편의점
+        String keyword = "세이노";
+
+        // when
+        List<Book> books = bookRepository.findByTitle(keyword);
+
+        // then
+        assertThat(books.get(0).getTitle()).as("책검색 결과 %s, 검색한 책 : %s",books.get(0).getTitle(), book2.getTitle()).isEqualTo(book.getTitle());
+    }
 }
