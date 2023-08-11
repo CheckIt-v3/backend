4457a9437aa85db7a5f8326498cffcc46733f2ce
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-08-11 13:18:12 +0900

    test: UserController 테스트코드 작성

--- src/test/java/com/techeer/checkIt/fixture/UserFixtures.java
+++ src/test/java/com/techeer/checkIt/fixture/UserFixtures.java
@@ -1,15 +1,45 @@
 package com.techeer.checkIt.fixture;
 
+import com.techeer.checkIt.domain.user.dto.request.UserJoinReq;
+import com.techeer.checkIt.domain.user.dto.request.UserLoginReq;
+import com.techeer.checkIt.domain.user.entity.Role;
 import com.techeer.checkIt.domain.user.entity.User;
+import com.techeer.checkIt.global.jwt.JwtToken;
 
 public class UserFixtures {
+    public static final UserJoinReq TEST_USER_JOIN_REQ =
+            UserJoinReq.builder()
+                    .username("test3")
+                    .password("@Test123")
+                    .nickname("테스트3")
+                    .build();
+
+    public static final UserLoginReq TEST_USER_LOGIN_REQ =
+            UserLoginReq.builder()
+                    .username("test")
+                    .password("@Test123")
+                    .build();
+
+    public static final JwtToken TEST_JWT =
+            JwtToken.builder()
+                    .grantType("Bearer")
+                    .accessToken("access 토큰")
+                    .refreshToken("refresh 토큰")
+                    .build();
+
     public static final User TEST_USER =
             User.builder()
-                    .id(1L)
+                    .username("test3")
+                    .password("@Test123")
+                    .nickname("테스트3")
+                    .role(Role.USER)
                     .build();
 
     public static final User TEST_USER2 =
             User.builder()
-                    .id(2L)
+                    .username("test2")
+                    .password("@Test123")
+                    .nickname("테스트2")
+                    .role(Role.USER)
                     .build();
 }
