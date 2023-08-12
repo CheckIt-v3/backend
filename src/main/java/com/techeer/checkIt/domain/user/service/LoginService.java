a44ccae85cc424763ca1123def7268c5f0c866db
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-08-13 02:56:57 +0900

    fix: redis 키 설정 수정

--- src/main/java/com/techeer/checkIt/domain/user/service/LoginService.java
+++ src/main/java/com/techeer/checkIt/domain/user/service/LoginService.java
@@ -42,32 +42,28 @@ public class LoginService {
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
         JwtToken jwt = jwtTokenProvider.generateToken(authentication);
 
-        //redis에 username을 key로 저장
-        redisTemplate.opsForValue().set("ID: " + user.getUsername(), jwt.getRefreshToken(), jwt.getRefreshTokenExpirationTime(), TimeUnit.MILLISECONDS);
+        //redis 에 username 을 key 로 저장, refresh 토큰을 value 값으로 저장
+        redisTemplate.opsForValue().set("RT:" + user.getUsername(), jwt.getRefreshToken(), jwt.getRefreshTokenExpirationTime(), TimeUnit.MILLISECONDS);
 
         return jwt;
     }
 
     public void logout(JwtToken jwtToken) {
-//        Authentication authentication = jwtTokenProvider.getAuthentication(jwtToken.getAccessToken());
-//        if (redisTemplate.opsForValue().get("ID: " + authentication.getName()) != null) {
-//            redisTemplate.delete("ID: " + authentication.getName()); //Token 삭제
-//        }
         // 로그아웃 하고 싶은 토큰이 유효한 지 먼저 검증하기
         if (!jwtTokenProvider.validateToken(jwtToken.getAccessToken())){
             throw new IllegalArgumentException("로그아웃 : 유효하지 않은 토큰입니다.");
         }
 
-        // Access Token에서 User email을 가져온다
+        // Access 토큰에서 User 정보를 가져온다
         Authentication authentication = jwtTokenProvider.getAuthentication(jwtToken.getAccessToken());
 
-        // Redis에서 해당 User email로 저장된 Refresh Token 이 있는지 여부를 확인 후에 있을 경우 삭제를 한다.
-        if (redisTemplate.opsForValue().get("ID: "+ authentication.getName()) != null){
-            // Refresh Token을 삭제
-            redisTemplate.delete("ID: " + authentication.getName());
+        // redis 에서 해당 User username 으로 저장된 Refresh Token 이 있는지 여부를 확인 후에 있을 경우 삭제를 한다.
+        if (redisTemplate.opsForValue().get("RT:" + authentication.getName()) != null){
+            // Refresh 토큰 삭제
+            redisTemplate.delete("RT:" + authentication.getName());
         }
 
-        // 해당 Access Token 유효시간을 가지고 와서 BlackList에 저장하기
+        // 해당 Access Token 유효시간을 가지고 와서 BlackList 에 저장
         Long expiration = jwtTokenProvider.getExpiration(jwtToken.getAccessToken());
         redisTemplate.opsForValue().set(jwtToken.getAccessToken(),"logout", expiration, TimeUnit.MILLISECONDS);
     }
