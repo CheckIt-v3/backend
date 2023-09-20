ca92555b9952d3ebf354c3aa95e927f92f53be8a
fnzl54 <rnjscksdud1@naver.com>
2023-09-20 16:02:17 +0900

    Revert "feat: 좋아요를 위해 기존 Redis 추가 연결"
    This reverts commit ef59a6c32be691ded73e8e96163dd07c792d4ce7.

--- src/main/java/com/techeer/checkIt/global/config/RedisRepositoryConfig.java
+++ src/main/java/com/techeer/checkIt/global/config/RedisRepositoryConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -28,15 +29,6 @@ public class RedisRepositoryConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        return redisTemplate;
-    }
-
-    @Bean
-    public RedisTemplate<String, String> redisLikeTemplate() {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory());
