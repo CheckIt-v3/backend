d91dffbdd1f547f8347ee50c3d42a29168d841d0
fnzl54 <rnjscksdud1@naver.com>
2023-09-24 18:22:16 +0900

    feat: Review 로직 추가 (API 구현 완료)

---
+++ src/main/java/com/techeer/checkIt/domain/review/controller/ReviewController.java
@@ -0,0 +1,84 @@
+package com.techeer.checkIt.domain.review.controller;
+
+import com.techeer.checkIt.domain.book.entity.Book;
+import com.techeer.checkIt.domain.book.service.BookService;
+import com.techeer.checkIt.domain.review.dto.request.CreateReviewReq;
+import com.techeer.checkIt.domain.review.dto.response.ReviewRes;
+import com.techeer.checkIt.domain.review.service.ReviewService;
+import com.techeer.checkIt.domain.user.entity.User;
+import com.techeer.checkIt.domain.user.entity.UserDetail;
+import com.techeer.checkIt.domain.user.service.UserService;
+import com.techeer.checkIt.global.result.ResultCode;
+import com.techeer.checkIt.global.result.ResultResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "리뷰 API")
+@RestController
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+  private  final ReviewService reviewService;
+  private final UserService userService;
+  private final BookService bookService;
+
+  @ApiOperation(value = "리뷰 생성 API")
+  @PostMapping()
+  public ResponseEntity<ResultResponse> createReview(
+      @AuthenticationPrincipal UserDetail userDetail,
+      @RequestBody CreateReviewReq createReviewReq
+  ){
+    User user = userService.findUserByUsername(userDetail.getUsername());
+    Book book = bookService.findById(createReviewReq.getBookId());
+    reviewService.createReview(user, book, createReviewReq);
+    ReviewRes reviewRes = reviewService.findReviewByNameIdBookId(user, createReviewReq.getBookId());
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.REVIEW_CREATE_SUCCESS, reviewRes));
+  }
+
+  @ApiOperation(value = "리뷰 조회 API")
+  @GetMapping("{bookId}")
+  public ResponseEntity<ResultResponse> getReviewByUserNameBookId(
+      @AuthenticationPrincipal UserDetail userDetail,
+      @PathVariable Long bookId
+  ){
+    User user = userService.findUserByUsername(userDetail.getUsername());
+    ReviewRes reviewRes = reviewService.findReviewByNameIdBookId(user, bookId);
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.REVIEW_CREATE_SUCCESS, reviewRes));
+  }
+
+  @ApiOperation(value = "리뷰 지우기 API")
+  @DeleteMapping ("{bookId}")
+  public ResponseEntity<ResultResponse> deleteReviewUserNameBookId(
+      @AuthenticationPrincipal UserDetail userDetail,
+      @PathVariable Long bookId
+  ){
+    User user = userService.findUserByUsername(userDetail.getUsername());
+    reviewService.deleteReview(user, bookId);
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.REVIEW_DELETE_SUCCESS));
+  }
+
+  @ApiOperation(value = "리뷰 내용 변경 API")
+  @PutMapping()
+  public ResponseEntity<ResultResponse> updateReview(
+      @AuthenticationPrincipal UserDetail userDetail,
+      @RequestBody CreateReviewReq createReviewReq
+  ) {
+    User user = userService.findUserByUsername(userDetail.getUsername());
+    reviewService.updateReview(user, createReviewReq);
+    ReviewRes reviewRes = reviewService.findReviewByNameIdBookId(user, createReviewReq.getBookId());
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.REVIEW_DELETE_SUCCESS, reviewRes));
+  }
+
+}
---
+++ src/main/java/com/techeer/checkIt/domain/review/service/ReviewService.java
@@ -0,0 +1,43 @@
+package com.techeer.checkIt.domain.review.service;
+
+import com.techeer.checkIt.domain.book.entity.Book;
+import com.techeer.checkIt.domain.review.dto.request.CreateReviewReq;
+import com.techeer.checkIt.domain.review.dto.response.ReviewRes;
+import com.techeer.checkIt.domain.review.entity.Review;
+import com.techeer.checkIt.domain.review.mapper.ReviewMapper;
+import com.techeer.checkIt.domain.review.exception.ReviewNotFoundException;
+import com.techeer.checkIt.domain.review.repository.ReviewRepository;
+import com.techeer.checkIt.domain.user.entity.User;
+import javax.transaction.Transactional;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewService {
+  private final ReviewRepository reviewRepository;
+  private final ReviewMapper reviewMapper;
+
+  public void createReview (User user, Book book, CreateReviewReq createReviewReq) {
+    Review review = reviewMapper.toEntity(user, book, createReviewReq.getTitle(), createReviewReq.getContent(), createReviewReq.getGrade());
+    reviewRepository.save(review);
+  }
+
+  public ReviewRes findReviewByNameIdBookId (User user, Long bookId) {
+    Review review = reviewRepository.findByUserNameBookId(user.getNickname(), bookId).orElseThrow(ReviewNotFoundException::new);
+    return  reviewMapper.toDto(review);
+  }
+
+  public void deleteReview (User user, Long bookId) {
+    Review review = reviewRepository.findByUserNameBookId(user.getNickname(), bookId).orElseThrow(ReviewNotFoundException::new);
+    review.delete();
+    //reviewRepository.save(review);
+  }
+
+  public void updateReview (User user, CreateReviewReq createReviewReq) {
+    Review review = reviewRepository.findByUserNameBookId(user.getNickname(), createReviewReq.getBookId()).orElseThrow(ReviewNotFoundException::new);
+    review.updateReview(createReviewReq);
+  }
+}
