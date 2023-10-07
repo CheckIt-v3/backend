47e8f124df3ff821c9f66eccecb7e4ec70aeaaf8
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-10-07 23:54:15 +0900

    fix: 채팅방 중복 예외처리 수정

--- src/main/java/com/techeer/checkIt/domain/chat/controller/ChatRoomController.java
+++ src/main/java/com/techeer/checkIt/domain/chat/controller/ChatRoomController.java
@@ -1,21 +1,27 @@
 package com.techeer.checkIt.domain.chat.controller;
 
+import com.techeer.checkIt.domain.chat.dto.response.ChatMessageRes;
 import com.techeer.checkIt.domain.chat.entity.ChatRoom;
 import com.techeer.checkIt.domain.chat.entity.UserChatRoom;
+import com.techeer.checkIt.domain.chat.exception.ChatRoomDuplicatedException;
 import com.techeer.checkIt.domain.chat.service.UserChatRoomService;
 import com.techeer.checkIt.domain.user.entity.UserDetail;
 import com.techeer.checkIt.global.result.ResultCode;
 import com.techeer.checkIt.global.result.ResultResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+import java.util.List;
+
+@Api(tags = "채팅 API")
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @RequestMapping("api/v1/chat/rooms")
@@ -23,16 +29,15 @@ public class ChatRoomController {
 
     private final UserChatRoomService userChatRoomService;
 
-    // 채팅하기 버튼 누르면 채팅방 생성
+    @ApiOperation(value = "채팅방 생성 API")
     @PostMapping
     public ResponseEntity<ResultResponse> createChatRoom (@AuthenticationPrincipal UserDetail userDetail) {
-
+        // TODO: 유저 채팅방 중복 예외처리 다시 해야함
+        if (userChatRoomService.duplicatedUserChatRoom(userDetail.getUser())) {
+            throw new ChatRoomDuplicatedException();
+        }
         ChatRoom chatRoom = userChatRoomService.createChatRoom();
         UserChatRoom userChatRoom = userChatRoomService.createUserChatRoom(userDetail.getUser(), chatRoom.getId());
-        // TODO: 유저 채팅방 중복 예외처리 해야함
-//        if (userChatRoomService.duplicatedUserChatRoom(userChatRoom)) {
-//            throw new ChatRoomDuplicatedException();
-//        }
         return ResponseEntity.ok(ResultResponse.of(ResultCode.CHATROOM_CREATE_SUCCESS, userChatRoom));
     }
 }
--- src/main/java/com/techeer/checkIt/domain/chat/repository/UserChatRoomRepository.java
+++ src/main/java/com/techeer/checkIt/domain/chat/repository/UserChatRoomRepository.java
@@ -1,15 +1,10 @@
 package com.techeer.checkIt.domain.chat.repository;
 
 import com.techeer.checkIt.domain.chat.entity.UserChatRoom;
-import com.techeer.checkIt.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface UserChatRoomRepository extends JpaRepository<UserChatRoom, Long> {
     boolean existsByUserId(Long userId);
-    List<UserChatRoom> findByUserId(Long userId);
-    User findUserByChatRoomId(Long ChatRoomId);
 }
