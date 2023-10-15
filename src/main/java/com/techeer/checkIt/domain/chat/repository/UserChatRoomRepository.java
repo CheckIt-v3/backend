c87eae356bc2ba698ebc9a2c626d5b9ff09eb6a4
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-10-16 00:23:27 +0900

    feat: 관리자 권한 추가 기능 구현

--- src/main/java/com/techeer/checkIt/domain/chat/repository/UserChatRoomRepository.java
+++ src/main/java/com/techeer/checkIt/domain/chat/repository/UserChatRoomRepository.java
@@ -4,7 +4,10 @@ import com.techeer.checkIt.domain.chat.entity.UserChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserChatRoomRepository extends JpaRepository<UserChatRoom, Long> {
     boolean existsByUserId(Long userId);
+    List<UserChatRoom> findByUserId(Long userId);
 }
