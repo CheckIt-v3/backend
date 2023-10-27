f3846aec828e01c51792afb8000b97302949e74a
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-10-27 20:53:41 +0900

    hotfix: cors 에러 해결

--- src/main/java/com/techeer/checkIt/global/config/WebConfiguration.java
+++ src/main/java/com/techeer/checkIt/global/config/WebConfiguration.java
@@ -10,7 +10,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
 }
