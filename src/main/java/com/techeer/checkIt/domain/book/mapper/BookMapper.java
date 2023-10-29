b2d0370fdc96785871e91c24905892391c59391f
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-10-29 19:14:57 +0900

    fix: logstash mapping 수정 및 book id 추가

--- src/main/java/com/techeer/checkIt/domain/book/mapper/BookMapper.java
+++ src/main/java/com/techeer/checkIt/domain/book/mapper/BookMapper.java
@@ -41,6 +41,7 @@ public class BookMapper {
     }
     public BookSearchLikeRes toBookSearchLikeDto(Book book) {
         return BookSearchLikeRes.builder()
+            .id(book.getId())
             .title(book.getTitle())
             .author(book.getAuthor())
             .publisher(book.getPublisher())
