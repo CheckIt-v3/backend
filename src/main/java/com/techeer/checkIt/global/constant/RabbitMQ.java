a23722b0ee2484412602b41723ffb5f3bdc32e80
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-11-08 23:48:09 +0900

    chore: rabbitmq 설정 추가

---
+++ src/main/java/com/techeer/checkIt/global/constant/RabbitMQ.java
@@ -0,0 +1,8 @@
+package com.techeer.checkIt.global.constant;
+
+public class RabbitMQ {
+
+    public static final String CHAT_QUEUE_NAME = "chat.queue";
+    public static final String CHAT_EXCHANGE_NAME = "chat.exchange";
+    public static final String ROUTING_KEY = "room.*";
+}
