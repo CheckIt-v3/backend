d404e476f27ee489bf2e36c6cf018a4ea48024a6
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-11-08 23:49:58 +0900

    feat: rabbitmq로 수정

--- src/main/java/com/techeer/checkIt/domain/chat/controller/ChatMessageController.java
+++ src/main/java/com/techeer/checkIt/domain/chat/controller/ChatMessageController.java
@@ -1,16 +1,16 @@
 package com.techeer.checkIt.domain.chat.controller;
 
 import com.techeer.checkIt.domain.chat.dto.request.CreateMessageReq;
-import com.techeer.checkIt.domain.chat.entity.ChatRoom;
-import com.techeer.checkIt.domain.chat.exception.ChatRoomDuplicatedException;
 import com.techeer.checkIt.domain.chat.service.UserChatRoomService;
-import com.techeer.checkIt.domain.user.entity.Role;
 import com.techeer.checkIt.domain.user.entity.UserDetail;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
@@ -18,22 +18,35 @@ import org.springframework.stereotype.Controller;
 import javax.validation.Valid;
 import java.util.Objects;
 
+import static com.techeer.checkIt.global.constant.RabbitMQ.*;
+
 @Controller
 @Slf4j
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatMessageController {
-    private final SimpMessageSendingOperations sendingOperations;
+    //    private final SimpMessageSendingOperations sendingOperations;
     private final UserChatRoomService userChatRoomService;
+    private final RabbitTemplate rabbitTemplate;
 
-    @MessageMapping("/chat/rooms/{chatRoomId}/message")
+    //    @MessageMapping("/chat/rooms/{chatRoomId}/message")
+    @MessageMapping("chat.message.{chatRoomId}")
     public void chat(StompHeaderAccessor headerAccessor,
                      @DestinationVariable Long chatRoomId,
-                     @Valid CreateMessageReq messageRequest) {
+                     @Payload CreateMessageReq messageRequest) {
         UserDetail userDetail = (UserDetail) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("userDetail");
 
-        sendingOperations.convertAndSend("/sub/chat/rooms/" + chatRoomId, messageRequest.getContent());
+//        sendingOperations.convertAndSend("/sub/chat/rooms/" + chatRoomId, messageRequest.getContent());
+        rabbitTemplate.convertAndSend(CHAT_EXCHANGE_NAME + "room." + chatRoomId, messageRequest.getContent());
+
+
         userChatRoomService.saveMessage(userDetail.getUser(), chatRoomId, messageRequest);
         log.info("Message [{}] send by user: {} to chatting room: {}", messageRequest.getContent(), userDetail.getUsername(), chatRoomId);
+    }
+
+    //메세지가 큐에 도착할 때 실행
+    @RabbitListener(queues = CHAT_QUEUE_NAME)
+    public void receive(CreateMessageReq messageRequest) {
+        log.info("message.getText = {}", messageRequest.getContent());
     }
 
 //    @MessageMapping("/enter/rooms/{chatRoomId}")
--- src/main/java/com/techeer/checkIt/global/config/WebSocketConfig.java
+++ src/main/java/com/techeer/checkIt/global/config/WebSocketConfig.java
@@ -3,15 +3,33 @@ package com.techeer.checkIt.global.config;
 import com.techeer.checkIt.global.stomp.StompExceptionHandler;
 import com.techeer.checkIt.global.stomp.StompHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.virtual-host}")
+    private String virtualHost;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+
     private final StompHandler stompHandler;
     private final StompExceptionHandler stompExceptionHandler;
 
@@ -25,8 +43,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
+        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
+//                .setRelayHost(host)
+//                .setRelayPort(port)
+//                .setSystemLogin(username)
+//                .setSystemPasscode(password)
+//                .setClientLogin(username)
+//                .setClientPasscode(password)
+//                .setVirtualHost(virtualHost);
         registry.setApplicationDestinationPrefixes("/pub");
+        registry.setPathMatcher(new AntPathMatcher("."));
     }
 
     @Override
