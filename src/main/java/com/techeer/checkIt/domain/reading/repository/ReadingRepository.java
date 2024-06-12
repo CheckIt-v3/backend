1dd1b509fd915a716ff0c8f50699f4f87d6b941a
gmlrude <qkrgmlrud00@gmail.com>
2024-06-12 18:17:46 +0900

    refactor: fetch join을 통한 N+1문제 해결

--- src/main/java/com/techeer/checkIt/domain/reading/repository/ReadingRepository.java
+++ src/main/java/com/techeer/checkIt/domain/reading/repository/ReadingRepository.java
@@ -5,6 +5,7 @@ import com.techeer.checkIt.domain.reading.entity.Reading;
 import com.techeer.checkIt.domain.reading.entity.ReadingStatus;
 import com.techeer.checkIt.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ReadingRepository extends JpaRepository<Reading,Long>, ReadingRepositoryCustom {
+    @Query("SELECT r FROM Reading r LEFT JOIN FETCH r.book WHERE r.user.id = :userId AND r.status = :status")
     List<Reading> findByUserIdAndStatus(@Param("userId") Long userId, @Param("status") ReadingStatus status);
     Optional<Reading> findByUserIdAndBookIdAndStatus(Long userId, Long bookId, ReadingStatus status);
     Optional<Reading> findByUserAndBook(User user, Book book);
