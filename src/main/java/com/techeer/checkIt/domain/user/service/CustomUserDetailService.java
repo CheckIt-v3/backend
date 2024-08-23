d36aba9a0ac1b298d52fa3e92ddc0c35081358b4
Heekyeong Park <qkrgmlrud00@gmail.com>
2024-08-23 20:21:19 +0900

    feat: loadUserByUsername 유저 캐싱 처리

--- src/main/java/com/techeer/checkIt/domain/user/service/CustomUserDetailService.java
+++ src/main/java/com/techeer/checkIt/domain/user/service/CustomUserDetailService.java
@@ -6,6 +6,7 @@ import com.techeer.checkIt.domain.user.exception.UserNotFoundException;
 import com.techeer.checkIt.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,6 +20,7 @@ public class CustomUserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
+    @Cacheable("users")
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("loadUserByUsername called for username: {}", username);
         User principal = userRepository.findUserByUsername(username)
