68d4118b65ff47cb4599a4b7c69592994448ecf2
Heekyeong Park <qkrgmlrud00@gmail.com>
2024-09-18 02:24:15 +0900

    hotfix: 프론트 배포 주소 cors 설정

--- src/main/java/com/techeer/checkIt/global/config/WebConfiguration.java
+++ src/main/java/com/techeer/checkIt/global/config/WebConfiguration.java
@@ -9,7 +9,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://check-it.shop")
+                .allowedOrigins("http://localhost:5173", "https://check-it.shop", "https://d1gntbefjuz72k.cloudfront.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
